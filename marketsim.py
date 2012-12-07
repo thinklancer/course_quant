e7ad0a36305e91c1f40ebef70f0ed86b4adf1c87
thinklancer <thinklancer@gmail.com>
2012-12-07 19:23:30 +0100

    middleway
    work done at school, not finished

--- marketsim.py
+++ marketsim.py
@@ -1,10 +1,57 @@
 #! /opt/local/bin/python
 
 import sys
+import matplotlib.pyplot as plt
+from pylab import *
+import datetime as dt
 
-def main(inital,orderfile,output)
+from qstkutil import DataAccess as da
+from qstkutil import qsdateutil as du
+from qstkutil import tsutil as tsu
+
+
+def main(inital,orderfile,output):
+    # read in the order book
     data = np.loadtxt(orderfile,dtype='I,I,I,S5,S4,I',delimiter=',')
+    ordersyms = []
+    order = []
+    amount = []
+    orderdate =[]
+    for i in data:
+        ordersyms.append(i[3])
+        order.append(i[4])
+        amount.append(i[5])
+        
+    # build portfolio
+    port = {}
+    for stock in ordersyms:
+        port.update({stock:0})
     
+    # check the validity
+    dataobj = da.DataAccess('Yahoo')
+    all_symbols = dataobj.get_all_symbols()
+    intersectsyms = list(set(all_symbols) & set(port.keys))
+    bydsyms = []
+    if size(intersectsyms) < size(port.keys):
+        badsyms = list(set(port.keys) - set(intersectsyms))
+        print "warning: portforlio contains symbols that do not exist:"
+        print badsyms
+        exit()
 
-if __name__ == "__main__"
-    main(argv[1],argv[2],argv[3])
+    # set the balance of the portfolio
+    print type(initial)
+    port.update({"cash":initial})
+
+    # looping date
+    startday = dt.datetime(2011,1,1)
+    endday = dt.datetime(2011,12,31)
+    timeofday = dt.timedelta(hours=16)
+    timestamps = du.getNYSEdays(startday,endday,timeofday)
+    close = dataobj.get_data(timestamps,port.keys,"close")
+
+    # excute order
+
+    # record balance
+
+if __name__ == "__main__":
+    main(sys.argv[1],sys.argv[2],sys.argv[3])
